72a4f4e101e61b08dca8073f3e88efb4fb3dc237
Daniel Galvis <34688953+danielgalvis98@users.noreply.github.com>
2022-05-26 11:16:32 -0500

    Initial commit

---
+++ PlaylistsWorkshop/Model/PlaylistWorkshopContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PlaylistsWorkshop.Model
+{
+    public class PlaylistWorkshopContext : DbContext
+    {
+
+        public DbSet<Playlist> Playlists { get; set; }
+        public DbSet<Song> Songs { get; set; }
+
+        public PlaylistWorkshopContext(DbContextOptions<PlaylistWorkshopContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Playlist>()
+                .HasMany(p => p.Songs)
+                .WithOne()
+                .IsRequired();
+        }
+    }
+}
